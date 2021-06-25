1cb8b5b507d0a1c3f2c4c7ffe8e5c03c9a934f41
Matthew Early <matthewjearly@gmail.com>
2021-06-25 11:50:44 -0400

    fix ref warning

--- openGLdemo/Utilities.h
+++ openGLdemo/Utilities.h
@@ -24,7 +24,8 @@ inline std::string ReadFileToString(const char* filepath) {
 
 inline const char* extract_prog_name(const char* full) {
   std::string p1 = full;
-  std::string p2 = p1.substr(p1.find_last_of("/\\") + 1,  // from after last slash
+  static std::string p2;
+  p2 = p1.substr(p1.find_last_of("/\\") + 1,  // from after last slash
     (p1.length() - p1.find_last_of("/\\")) - (p1.length() - (p1.find_last_of(".")) + 1)  // up to last dot
   );
   return p2.c_str();
